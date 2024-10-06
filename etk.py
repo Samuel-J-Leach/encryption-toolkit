06dc8409fc8434bacdbcbe66abb7ed939406b10b
Samuel-J-Leach <samueljleach@outlook.com>
2024-10-06 23:52:46 +0100

    added enryption of file content and keys from filepaths

--- etk.py
+++ etk.py
@@ -8,13 +8,21 @@ def readHelpFile():
 def main():
     command = sys.argv[1]
     if command == "help":
-        output = readHelpFile()
-    elif command == "fernet":
-        output = run_fernet(sys.argv[2:])
-    elif command == "rsa":
-        output = "this feature has not been implemented yet"
-    else:
-        output = "ERROR: command '"+command+"' not recognised, enter 'python etk.py help' for a list of valid commands"
+        return readHelpFile()
+
+    flags = []
+    inputs = []
+    if len(sys.argv) >= 3:
+        if sys.argv[2][0] == "-":
+            if "s" in sys.argv[2]: flags.append("s")#save output to file
+            if "k" in sys.argv[2]: flags.append("k")#take key from file
+            if "i" in sys.argv[2]: flags.append("i")#take input from file
+            inputs = sys.argv[3:]
+        else:
+            inputs = sys.argv[2:]
+        
+    output = run_fernet(command, flags, inputs)
     return output
 
 print("\n" + main())
+#command format: python etk.py encrypt {key} message to be encrypted
--- fernet.py
+++ fernet.py
@@ -13,14 +13,31 @@ def decrypt(key, ciphertext):
 def create_key():
     return Fernet.generate_key().decode(FORMAT)
 
-def run_fernet(args):
-    operation = args[0]
+def run_fernet(operation, flags, args):
+    kstart = -1
+    kend = -1
+    for i in range(len(args)):
+        if args[i][0] == "{": kstart = i
+        if args[i][len(args[i])-1] == "}": kend = i+1
+
+    if (kstart == -1 or kend == -1 or kstart >= kend) and operation != "key": return "ERROR: command format is incorrect.\nEnter 'python etk.py help' for help"
+    
+    if "k" in flags:
+        key = (open(" ".join(args[kstart:kend]))).read()
+    else:
+        key = " ".join(args[kstart:kend])
+
+    if "i" in flags:
+        data = (open(" ".join(args[kend:]))).read()
+    else:
+        data = " ".join(args[kend:])
+    
     if operation == "encrypt":
-        output = encrypt(args[1], " ".join(sys.argv[2:]))
+        output = encrypt(key, data)
     elif operation == "decrypt":
-        output = decrypt(args[1], " ".join(sys.argv[2:]))
+        output = decrypt(key, data)
     elif operation == "key":
         output = create_key()
     else:
-        output = "ERROR: operation '"+operation+"' not recognised, enter 'python etk.py help' for a list of valid operations"
+        output = "ERROR: operation '"+operation+"' not recognised.\nEnter 'python etk.py help' for a list of valid operations"
     return output
