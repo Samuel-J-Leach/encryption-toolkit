86e786bcf733b7d35d20951414df2ba4b3a24e15
Samuel-J-Leach <samueljleach@outlook.com>
2024-10-07 15:46:21 +0100

    added output files, fixed errors, organised code, added comments

--- etk.py
+++ etk.py
@@ -1,27 +1,56 @@
 import sys
+import os
 from fernet import run_fernet
 
 def readHelpFile():
     file = open("help.txt","r")
     return file.read()
 
-def main():
-    command = sys.argv[1]
-    if command == "help":
-        return readHelpFile()
+def saveOutput(command, flags, output):
+    filename = "output_files/etk-"+command+"-output-"+("".join(flags))+"-"
+    i = 0
+    path = ""
+    while True:
+        try:
+            file = open(filename+str(i), "x")
+            file.write(output)
+            path = str(os.path.abspath(filename+str(i)))
+            file.close()
+            break
+        except:
+            i += 1
+            if i == 100:
+                return "error"
+    return "output saved to: "+path
 
+def parseInput(args):
+    command = args[1]
     flags = []
     inputs = []
-    if len(sys.argv) >= 3:
-        if sys.argv[2][0] == "-":
-            if "s" in sys.argv[2]: flags.append("s")#save output to file
-            if "k" in sys.argv[2]: flags.append("k")#take key from file
-            if "i" in sys.argv[2]: flags.append("i")#take input from file
-            inputs = sys.argv[3:]
+    if len(args) >= 3:
+        if args[2][0] == "-":
+            if "s" in args[2]: flags.append("s")#save output to file
+            if "k" in args[2]: flags.append("k")#take key from file
+            if "i" in args[2]: flags.append("i")#take input from file
+            x = 1
         else:
-            inputs = sys.argv[2:]
-        
-    output = run_fernet(command, flags, inputs)
+            x = 0
+        try:
+            inputs = args[2+x:]
+        except:
+            inputs = []
+    return (command, flags, inputs)
+
+def main():
+    command, flags, inputs = parseInput(sys.argv)
+    
+    if command == "help":
+        output =  readHelpFile()
+    else:
+        output = run_fernet(command, flags, inputs)
+    
+    if "s" in flags:
+        output = output + "\n\n" + saveOutput(command, flags, output)
     return output
 
 print("\n" + main())
--- fernet.py
+++ fernet.py
@@ -13,31 +13,49 @@ def decrypt(key, ciphertext):
 def create_key():
     return Fernet.generate_key().decode(FORMAT)
 
-def run_fernet(operation, flags, args):
-    kstart = -1
-    kend = -1
+#determines which items in args make up the key
+def findKey(args):
+    start = -1
+    end = -1
     for i in range(len(args)):
-        if args[i][0] == "{": kstart = i
-        if args[i][len(args[i])-1] == "}": kend = i+1
+        if args[i][0] == "{": start = i
+        if args[i][len(args[i])-1] == "}": end = i+1
+    return (start, end)
 
-    if (kstart == -1 or kend == -1 or kstart >= kend) and operation != "key": return "ERROR: command format is incorrect.\nEnter 'python etk.py help' for help"
-    
+#either retrieves key from args or from a file
+def retrieveKey(flags, args, start, end):
     if "k" in flags:
-        key = (open(" ".join(args[kstart:kend]))).read()
+        path = " ".join(args[start:end])
+        key = (open(path[1:len(path)-1])).read()
     else:
-        key = " ".join(args[kstart:kend])
+        key = " ".join(args[start:end])
+        key = key[1:len(key)-1]
+    return key
 
+#either retrieves input data from args or from a file
+def retrieveData(flags, args, start):
     if "i" in flags:
-        data = (open(" ".join(args[kend:]))).read()
+        data = (open(" ".join(args[start:]))).read()
     else:
-        data = " ".join(args[kend:])
+        data = " ".join(args[start:])
+    return data
+
+def run_fernet(operation, flags, args):
     
-    if operation == "encrypt":
-        output = encrypt(key, data)
-    elif operation == "decrypt":
-        output = decrypt(key, data)
-    elif operation == "key":
-        output = create_key()
+    kstart, kend = findKey(args)
+    if (kstart == -1 or kend == -1 or kstart >= kend) and operation != "key":
+        output = "ERROR: command format is incorrect.\nEnter 'python etk.py help' for help"
     else:
-        output = "ERROR: operation '"+operation+"' not recognised.\nEnter 'python etk.py help' for a list of valid operations"
+        key = retrieveKey(flags, args, kstart, kend)
+        data = retrieveData(flags, args, kend)
+        
+        if operation == "encrypt":
+            output = encrypt(key, data)
+        elif operation == "decrypt":
+            output = decrypt(key, data)
+        elif operation == "key":
+            output = create_key()
+        else:
+            output = "ERROR: operation '"+operation+"' not recognised.\nEnter 'python etk.py help' for a list of valid operations"
+    
     return output
